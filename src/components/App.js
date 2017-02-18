5779f5a52cea26607cbc4fa4302b3cced9ac4576
Don Denton <don@happycollision.com>
2017-02-18 17:09:36 -0600

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -8,20 +8,6 @@ import {makeParts} from '../utils/textInterpreter';
 //styles
 import './App.scss';
 
-// const swipeOptions = {
-//   startSlide: startSlide < paneNodes.length && startSlide >= 0 ? startSlide : 0,
-//   auto: parseInt(query.auto, 10) || 0,
-//   speed: parseInt(query.speed, 10) || 300,
-//   disableScroll: query.disableScroll === 'true',
-//   continuous: query.continuous === 'true',
-//   callback() {
-//     console.log('slide changed');
-//   },
-//   transitionEnd() {
-//     console.log('ended transition');
-//   }
-// };
-
 class App extends Component {
   constructor(props) {
     super(props)
