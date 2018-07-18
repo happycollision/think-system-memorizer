cbb893d30ec6072692851540cef2a95642e50e36
Don Denton <don@happycollision.com>
2018-07-17 23:56:10 -0500

    create libretto show route for testing libretto component
    though currently empty, it will have data later

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('librettos', function() {
+    this.route('show', {path: '/:dasherized_name'});
+  });
 });
 
 export default Router;
