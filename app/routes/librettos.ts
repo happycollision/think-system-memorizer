0fd62aac57750bd2984605763783826f27c51e51
Don Denton <don@happycollision.com>
2018-07-18 23:44:30 -0500

    Add other librettos

--- app/routes/librettos.ts
+++ app/routes/librettos.ts
@@ -8,7 +8,13 @@ export interface ILibrettoListing {
 export default class LibrettosRoute extends Route {
   model(): ILibrettoListing[] {
     return [
-      {label: 'Bridges: Robert', file: '/files/Bridges.txt'}
+      {label: 'Hairspray: Corny', file: '/files/Hairspray.txt'},
+      {label: 'Bridges: Robert', file: '/files/Bridges.txt'},
+      {label: 'Elf: Jovie', file: '/files/Elf.txt'},
+      {label: 'Grease: Kenicke', file: '/files/Grease.txt'},
+      {label: 'Zombie Prom: Jonny', file: '/files/ZombieProm.txt'},
+      {label: 'Music Man: Harold', file: '/files/MusicManHaroldHill.txt'},
+      {label: 'Music Man: Marian', file: '/files/MusicManMarianHill.txt'},
     ]
   }
 }
