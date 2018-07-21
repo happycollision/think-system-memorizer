3c0186ff5963cc66f342071eb018d6cf8e9b2e3c
Don Denton <don@happycollision.com>
2018-07-21 00:29:48 -0500

    Actual html elements for card content

--- app/controllers/librettos/show.ts
+++ app/controllers/librettos/show.ts
@@ -2,10 +2,16 @@ import Controller from '@ember/controller';
 import { computed } from '@ember-decorators/object';
 import { makeParts } from '../../utils/textInterpreter';
 
+function createEl(str: string): HTMLElement {
+  const el = document.createElement('div');
+  el.innerHTML = str;
+  return el;
+}
+
 export default class LibrettosShowController extends Controller {
-  @computed('model') get cards(): Array<{front: string, back: string, isFlipped: boolean}> {
+  @computed('model') get cards(): Array<{front: HTMLElement, back: HTMLElement, isFlipped: boolean}> {
     return makeParts(this.get('model').text).map(tuple => {
-      const [front, back] = tuple;
+      const [front, back] = tuple.map(str => createEl(str));
       return {front, back, isFlipped: false};
     })
   }
