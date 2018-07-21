597080a43e23c15ba970ecbeccf35b772fdb2b23
Don Denton <don@happycollision.com>
2018-07-20 21:46:06 -0500

    use ember-test-selectors properly for testing

--- tests/integration/components/flip-card-test.ts
+++ tests/integration/components/flip-card-test.ts
@@ -12,20 +12,20 @@ module('Integration | Component | flip-card', function(hooks) {
 
     await render(hbs`<FlipCard as |card|><card.front>Hello</card.front><card.back>There</card.back></FlipCard>`);
 
-    assert.dom('[data-test-card-side]:first-child').hasText('Hello')
-    assert.dom('[data-test-card-side]:last-child').hasText('There')
+    assert.dom('[data-test-card-front]').hasText('Hello')
+    assert.dom('[data-test-card-back]').hasText('There')
   });
 
-  test('it renders the back of a card only when flipped', async function(assert) {
+  test('it adds the flipped class when flipped (so css works)', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
     this.set('flipped', false);
     
     await render(hbs`<FlipCard @flipped={{flipped}} as |card|><card.back>Hello</card.back></FlipCard>`);
-    assert.dom(this.element.firstChild).hasNoClass('flipped', 'Back starts hidden');
+    assert.dom('[data-test-flip-card]').hasNoClass('flipped', 'Back starts hidden');
 
     this.set('flipped', true);
      
-    assert.dom(this.element.firstChild).hasClass('flipped', 'Back becomes visible');
+    assert.dom('[data-test-flip-card]').hasClass('flipped', 'Back becomes visible');
   })
 });
