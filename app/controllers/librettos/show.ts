0f8a33cb3af9d3f5dba2d636af52861331e6ed92
Don Denton <don@happycollision.com>
2018-07-21 00:14:57 -0500

    add rudimentary data loading

--- app/controllers/librettos/show.ts
+++ app/controllers/librettos/show.ts
@@ -1,13 +1,12 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember-decorators/object';
+import { makeParts } from '../../utils/textInterpreter';
 
 export default class LibrettosShowController extends Controller {
-  @computed('model') get cards() { return [
-    {front: 'Content for Front 1', back: 'content for back', isFlipped: false},
-    {front: 'Content for Front 2', back: 'content for back', isFlipped: false},
-    {front: 'Content for Front 3', back: 'content for back', isFlipped: false},
-    {front: 'Content for Front 4', back: 'content for back', isFlipped: false},
-    {front: 'Content for Front 5', back: 'content for back', isFlipped: false},
-    {front: 'Content for Front 6', back: 'content for back', isFlipped: false},
-  ]}
+  @computed('model') get cards(): Array<{front: string, back: string, isFlipped: boolean}> {
+    return makeParts(this.get('model').text).map(tuple => {
+      const [front, back] = tuple;
+      return {front, back, isFlipped: false};
+    })
+  }
 }
--- app/routes/librettos/show.ts
+++ app/routes/librettos/show.ts
@@ -1,10 +1,16 @@
 import Route from '@ember/routing/route';
-import {slugify} from '../../helpers/slugify';
+import { slugify } from '../../helpers/slugify';
 import { ILibrettoListing } from 'think-system-memorizer/routes/librettos';
+import { hash } from 'rsvp';
 
 export default class LibrettosShowRoute extends Route {
   model({dasherized_name}: {dasherized_name: string}) {
     const librettos: ILibrettoListing[] = this.modelFor('librettos') as ILibrettoListing[];
-    return librettos.find(lib => slugify([lib.label]) === dasherized_name);
+    const metadata = librettos.find(lib => slugify([lib.label]) === dasherized_name);
+    if (!metadata) { throw new Error('no metadata found for libretto route ' + dasherized_name)}
+    return hash({
+      metadata,
+      text: fetch(metadata.file).then(response => response.text()),
+    })
   }
 }
