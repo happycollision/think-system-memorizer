68063ead03c77a43a47453f1ae44e71f5ba0cf3b
Don Denton <don@happycollision.com>
2018-07-29 13:13:31 -0500

    use proper rooturl for github pages

--- config/environment.js
+++ config/environment.js
@@ -46,6 +46,7 @@ module.exports = function(environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
     ENV.locationType = 'hash'; // for GhPages
+    ENV.rootURL = '/think-system-memorizer/';
   }
 
   return ENV;
