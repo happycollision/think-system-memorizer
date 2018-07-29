35d0700549e584fb111ce671a37dfc5b57bc763a
Don Denton <don@happycollision.com>
2018-07-29 14:53:44 -0500

    remove all files to prep for switch to ember

--- src/actions/StateActions.js
+++
@@ -1,8 +0,0 @@
-import dispatcher from '../utils/dispatcher';
-
-export function setState (state, doNotSet) {
-  if (doNotSet !== undefined) {
-    throw new Error('It looks like you made a call to `StateActions.setState` when you thought you were calling `this.setState`. `StateActions.setState` doesn\'t fire any callback functions.')
-  }
-  dispatcher.dispatch({type: 'SET_STATE', state});
-}
--- src/config.js
+++
@@ -1,1 +0,0 @@
-export var baseurl = process.env.PUBLIC_URL || '';
