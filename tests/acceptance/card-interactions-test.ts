ff7ff3d041416dc95fb02031855505b9ca10f5d2
Don Denton <don@happycollision.com>
2018-07-29 01:48:51 -0500

    Application test for sequencial memorize

--- tests/acceptance/card-interactions-test.ts
+++ tests/acceptance/card-interactions-test.ts
@@ -22,4 +22,57 @@ module('Acceptance | card interactions', function(hooks) {
     await click('[data-test-previous-card]')
     assert.notOk(currentURL().match('card=2'))
   });
+
+  test('re-memorize sequencially steps through cards', async function(assert) {
+    await visit('/librettos/elf-jovie?view=cards');
+    const redux = this.owner.lookup('service:redux');
+    const currentState = () => redux.getState();
+
+    await click('[data-test-sequencial-memorize]');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[0].isFlipped, true, 'the first card gets flipped');
+
+    await click('[data-test-sequencial-memorize]');
+    assert.ok(currentURL().match('card=2'), 'The second card is showing');
+    await click('[data-test-sequencial-memorize]');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[1].isFlipped, true, 'the second card gets flipped');
+
+    await click('[data-test-sequencial-memorize]');
+    assert.ok(currentURL().match('card=3'), 'The third card is showing');
+    await click('[data-test-sequencial-memorize]');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[2].isFlipped, true, 'the third card gets flipped');
+
+    await click('[data-test-sequencial-memorize]');
+    assert.ok(currentURL().match('card=4'), 'The fourth card is showing');
+    await click('[data-test-sequencial-memorize]');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[3].isFlipped, true, 'the fourth card gets flipped');
+
+    await click('[data-test-sequencial-memorize]');
+    assert.ok(currentURL().match('card=2'), 'The second card is showing again');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[1].isFlipped, false, 'the second card is no longer flipped');
+    await click('[data-test-sequencial-memorize]');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[1].isFlipped, true, 'the second card is flipped again');
+
+    await click('[data-test-sequencial-memorize]');
+    assert.ok(currentURL().match('card=3'), 'The third card is showing again');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[2].isFlipped, false, 'the third card is no longer flipped');
+    await click('[data-test-sequencial-memorize]');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[2].isFlipped, true, 'the third card is flipped again');
+
+    await click('[data-test-sequencial-memorize]');
+    assert.ok(currentURL().match('card=4'), 'The fourth card is showing again');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[3].isFlipped, false, 'the fourth card is no longer flipped');
+    await click('[data-test-sequencial-memorize]');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[3].isFlipped, true, 'the fourth card is flipped again');
+
+    await click('[data-test-sequencial-memorize]');
+    assert.ok(currentURL().match('card=5'), 'The fifth card is showing');
+    await click('[data-test-sequencial-memorize]');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[4].isFlipped, true, 'the fifth card gets flipped');
+
+    await click('[data-test-sequencial-memorize]');
+    assert.ok(currentURL().match('card=3'), 'The third card is showing again');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[2].isFlipped, false, 'the third card is no longer flipped');
+    await click('[data-test-sequencial-memorize]');
+    assert.equal(currentState().cardDecks['Elf: Jovie'].cards[2].isFlipped, true, 'the third card is flipped again');
+  })
 });
