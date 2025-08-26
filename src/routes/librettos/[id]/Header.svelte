ee0b489b37edaaa95600477ae870b6ff4c2f5d61
Don Denton <don@happycollision.com>
2025-08-25 22:38:23 -0500

    Add header padding considerations

--- src/routes/librettos/[id]/Header.svelte
+++ src/routes/librettos/[id]/Header.svelte
@@ -1,3 +1,14 @@
+<script module lang="ts">
+	const HEADER_PADDING = 8;
+
+	export function getHeaderHeightAndPadding() {
+		const headerHeight = getComputedStyle(document.documentElement).getPropertyValue(
+			'--header-height',
+		);
+		return (Number(headerHeight.replace('px', '')) || 0) + HEADER_PADDING;
+	}
+</script>
+
 <script lang="ts">
 	import { base } from '$app/paths';
 	import type { Libretto } from '../../api/librettos.json/+server';
@@ -16,7 +27,29 @@
 	let nextValue = $derived(cardStore.currentCardIndex + 1);
 </script>
 
-<div class="sticky top-0 isolate z-10 bg-gray-200 dark:bg-gray-800">
+<div
+	{@attach (el) => {
+		const style = document.createElement('style');
+		style.innerHTML = `
+  body * {
+    scroll-margin-top: calc(var(--header-height, 0) * 1px + ${HEADER_PADDING}px);
+  }
+`;
+		document.head.appendChild(style);
+
+		const updateHeaderHeight = () => {
+			document.documentElement.style.setProperty('--header-height', `${el.offsetHeight}`);
+		};
+		updateHeaderHeight();
+		window.addEventListener('resize', updateHeaderHeight);
+
+		return () => {
+			window.removeEventListener('resize', updateHeaderHeight);
+			style.remove();
+		};
+	}}
+	class="sticky top-0 isolate z-10 bg-gray-200 dark:bg-gray-800"
+>
 	<div class="flex justify-between gap-2 p-2">
 		<a class="btn" href="{base}/librettos/">
 			<span>Back <span class="sr-only sm:not-sr-only">to libretto list</span></span>
