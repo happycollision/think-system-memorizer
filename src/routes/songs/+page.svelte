36f1b256977b73f07275e37e88b5c0d75c14729f
Don Denton <don@happycollision.com>
2025-09-03 00:57:15 -0500

    list all loops on index page

--- src/routes/songs/+page.svelte
+++ src/routes/songs/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { base } from '$app/paths';
 	import { page } from '$app/state';
-	import { getSongs } from './db';
+	import Song from './[id]/Song.svelte';
+	import { getSongsWithLoops } from './db';
 
-	// get list of songs from indexdb store
-
-	const songs = getSongs();
-	const currentPage = $derived(page.url.pathname);
+	let songs = getSongsWithLoops();
+	let currentPage = $derived(page.url.pathname);
+	let songsWithLoops = $derived($songs?.filter((s) => s.loops.length > 0) || []);
+	let editingList = $state(false);
 </script>
 
 <svelte:head>
@@ -15,19 +16,41 @@
 
 <a href="{base}/librettos" class="mb-6 inline-block text-blue-600 hover:underline">Librettos</a>
 
+<!-- add a switch here for the editingList boolean -->
+<button
+	class="mb-6 ml-4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+	onclick={() => (editingList = !editingList)}
+>
+	{editingList ? 'View Songs' : 'Edit Songs'}
+</button>
+
 <div class="mx-auto max-w-6xl p-8">
-	<div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-		{#each $songs as song (song.name)}
-			<div
-				class="relative isolate rounded-lg border border-gray-200 bg-white p-6 shadow-sm transition-all hover:bg-gray-200 hover:shadow-md dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-700"
-			>
-				<h3 class="mb-2 text-xl font-semibold text-gray-800 dark:text-gray-200">
-					<a href="{currentPage}/{song.id}">
-						{song.name}
-						<span class="absolute inset-0"></span>
-					</a>
-				</h3>
-			</div>
-		{/each}
-	</div>
+	{#if editingList}
+		<div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+			{#each $songs as song (song.name)}
+				<div
+					class="relative isolate rounded-lg border border-gray-200 bg-white p-6 shadow-sm transition-all hover:bg-gray-200 hover:shadow-md dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-700"
+				>
+					<h3 class="mb-2 text-xl font-semibold text-gray-800 dark:text-gray-200">
+						<a href="{currentPage}/{song.id}">
+							{song.name}
+							<span class="absolute inset-0"></span>
+						</a>
+					</h3>
+				</div>
+			{/each}
+		</div>
+	{:else}
+		<div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+			{#each songsWithLoops as song (song.name)}
+				<div
+					class="relative isolate rounded-lg border border-gray-200 bg-white p-6 shadow-sm transition-all dark:border-gray-600 dark:bg-gray-800"
+				>
+					<h3 class="mb-2 text-xl font-semibold text-gray-800 dark:text-gray-200">
+						<Song songData={song} />
+					</h3>
+				</div>
+			{/each}
+		</div>
+	{/if}
 </div>
